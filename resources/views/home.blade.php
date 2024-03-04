7e6f95f7595d37a1e21e07d03611862988d2cbd8
khfiii <khfii635@gmail.com>
2024-03-04 21:27:16 +0800

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<x-layout-main>
+
+    <div class="navbar bg-base-100">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl">{{ config('app.name') }}</a>
+        </div>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal px-1 font-poppins font-medium">
+            <li class="hover:text-[#c04cfb]"><a>Home</a></li>
+            <li class="hover:text-[#c04cfb]"><a>Produk</a></li>
+            <li class="hover:text-[#c04cfb]"><a>Testimoni</a></li>
+            <li class="hover:text-[#c04cfb]"><a>Tentang</a></li>
+            <li class="hover:text-[#c04cfb]"><a>Kontak</a></li>
+          </ul>
+        </div>
+      </div>
+
+</x-layout-main>
